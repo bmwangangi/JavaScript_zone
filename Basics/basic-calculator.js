4b3820ac65310363ef98a30ed921a54e5d20fdf5
bmwangangi <trabbart8@gmail.com>
2024-12-05 22:24:58 +0300

    javascript zone

---
+++ Basics/basic-calculator.js
@@ -0,0 +1,9 @@
+// Declare variables
+let a = 20;
+let b = 4;
+
+// Perform arithmetic operations
+console.log("Addition (a + b):", a + b);
+console.log("Subtraction (a - b):", a - b);
+console.log("Multiplication (a * b):", a * b);
+console.log("Division (a / b):", a / b);
