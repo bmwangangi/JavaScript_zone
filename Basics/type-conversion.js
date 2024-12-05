a669871c0af4469a91c6f79efb6f86b9891da8b5
bmwangangi <trabbart8@gmail.com>
2024-12-05 21:53:51 +0300

    javascript zone

---
+++ Basics/type-conversion.js
@@ -0,0 +1,15 @@
+// Declare variables
+let num = "25"; // String
+console.log("Before Conversion:", num, ", Type:", typeof num);
+
+// Convert to a number
+num = Number(num);
+console.log("After Conversion:", num, ", Type:", typeof num);
+
+// Declare a boolean
+let isAvailable = true; // Boolean
+console.log("Before Conversion:", isAvailable, ", Type:", typeof isAvailable);
+
+// Convert to a string
+isAvailable = String(isAvailable);
+console.log("After Conversion:", isAvailable, ", Type:", typeof isAvailable);
