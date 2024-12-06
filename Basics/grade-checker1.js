9bd482aeb01e97eb5bc27634b44a7a1e1e9d63d8
bmwangangi <trabbart8@gmail.com>
2024-12-06 10:32:24 +0300

    switch statement

---
+++ Basics/grade-checker1.js
@@ -0,0 +1,23 @@
+// Declare the grade
+let grade = "B";
+
+// Use a switch statement to print the grade message
+switch (grade) {
+  case "A":
+    console.log("Excellent");
+    break;
+  case "B":
+    console.log("Good");
+    break;
+  case "C":
+    console.log("Average");
+    break;
+  case "D":
+    console.log("Poor");
+    break;
+  case "F":
+    console.log("Fail");
+    break;
+  default:
+    console.log("Invalid grade");
+}
