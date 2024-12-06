813373907868761c83e37d170e3d3b9f2d52b177
bmwangangi <trabbart8@gmail.com>
2024-12-06 10:14:31 +0300

    relational operators

---
+++ Basics/grade-checker.js
@@ -0,0 +1,11 @@
+// Declare a score
+let score = 85;
+
+// Check the grade
+if (score >= 90) {
+  console.log("Grade: A");
+} else if (score >= 70) {
+  console.log("Grade: B");
+} else {
+  console.log("Grade: Fail");
+}
