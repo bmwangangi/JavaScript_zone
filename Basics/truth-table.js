7b4f2e61bfa1d4c0cb7e235ed4ca2b95c1515600
bmwangangi <trabbart8@gmail.com>
2024-12-06 09:56:32 +0300

    truth table checkwith logicaloperators

---
+++ Basics/truth-table.js
@@ -0,0 +1,6 @@
+let isSunny = true;
+let isWeekend = false;
+
+console.log('it is sunny and the weekend', isSunny && isWeekend);
+console.log("it is sunny or the weekend", isSunny || isWeekend);
+console.log("is it not sunny", !isSunny);
