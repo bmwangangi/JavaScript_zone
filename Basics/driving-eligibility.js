dafd551365c0bc083fcf1b0364a69f42af2e75fc
bmwangangi <trabbart8@gmail.com>
2024-12-06 10:28:02 +0300

    ternary operator

---
+++ Basics/driving-eligibility.js
@@ -0,0 +1,6 @@
+// Declare the age
+let age = 20;
+
+// Use a ternary operator to check driving eligibility
+let eligibility = age >= 16 ? "Eligible to drive" : "Not eligible to drive";
+console.log(eligibility);
