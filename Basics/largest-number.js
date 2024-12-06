4f76c901eb0acae818a19b38fd25b3e1e14884c5
bmwangangi <trabbart8@gmail.com>
2024-12-06 10:19:10 +0300

    if else statement

---
+++ Basics/largest-number.js
@@ -0,0 +1,11 @@
+// Declare two numbers
+let num1 = 15;
+let num2 = 25;
+
+// Compare the numbers
+if (num1 > num2) {
+  console.log("num1 is larger");
+} else {
+  console.log("num2 is larger");
+}
+
