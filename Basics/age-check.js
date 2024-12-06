95f689fa360525c960905b751024f62612e00db4
bmwangangi <trabbart8@gmail.com>
2024-12-06 09:59:05 +0300

    age check with logical operators

---
+++ Basics/age-check.js
@@ -0,0 +1,6 @@
+// Declare a variable
+let age = 15;
+
+// Check conditions
+console.log("Is the person a teenager?", age >= 13 && age <= 19);
+console.log("Is the person a child or a senior?", age < 13 || age > 65);
