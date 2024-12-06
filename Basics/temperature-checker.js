fe2ec132f50c4c06211705e8fa540490fe33876c
bmwangangi <trabbart8@gmail.com>
2024-12-06 10:19:44 +0300

    if else statement

---
+++ Basics/temperature-checker.js
@@ -0,0 +1,11 @@
+// Declare the temperature
+let temperature = 25;
+
+// Check the temperature range
+if (temperature > 30) {
+  console.log("It's hot");
+} else if (temperature < 15) {
+  console.log("It's cold");
+} else {
+  console.log("It's moderate");
+}
