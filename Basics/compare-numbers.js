0deae9344839fe72d252e1d9b9836f2d74323596
bmwangangi <trabbart8@gmail.com>
2024-12-06 10:12:08 +0300

    relational operators

---
+++ Basics/compare-numbers.js
@@ -0,0 +1,7 @@
+let x = 5;
+let y = 10;
+
+console.log("Is x greater than y?", x > y);
+console.log("Is x less than or equal to y?", x <= y);
+console.log("Are x and y equal?", x == y);
+console.log("Are x and y not equal?", x != y);
