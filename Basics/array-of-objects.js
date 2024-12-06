443025631f6ebb4cfa78fcf365126dec44bb0a5f
bmwangangi <trabbart8@gmail.com>
2024-12-06 10:50:26 +0300

    objects

---
+++ Basics/array-of-objects.js
@@ -0,0 +1,9 @@
+let students = [
+  { name: "Alice", age: 20, grade: "A" },
+  { name: "Bob", age: 22, grade: "B" },
+  { name: "Charlie", age: 21, grade: "C" }
+];
+
+for (let student of students) {
+  console.log(`${student.name}: Grade ${student.grade}`);
+}
