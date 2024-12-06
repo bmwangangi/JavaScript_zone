0cab4a314424851bb31e8b4083521b6824a3da57
bmwangangi <trabbart8@gmail.com>
2024-12-06 10:46:03 +0300

    for loop

---
+++ Basics/object-properties.js
@@ -0,0 +1,12 @@
+let person = {
+  name: "John Doe",
+  age: 30,
+  isStudent: false
+};
+
+console.log("Name:", person.name);
+console.log("Age:", person.age);
+
+person.age = 31;
+
+console.log("Updated Person:", person);
